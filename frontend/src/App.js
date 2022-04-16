fbfc0ae002e549aa68797910198ec737694f471d
Joey Storer <joeystorer@gmail.com>
2022-04-16 18:47:42 -0400

    adding rating component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Homescreen from './screens/Homescreen';
+import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Homescreen from "./screens/Homescreen";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <Homescreen/>
+          <Homescreen />
         </Container>
       </main>
       <Footer />
